73bc72d82342ddeef1fef6183122f9ed5344e039
rayanccastro <56481997+rayanccastro@users.noreply.github.com>
2026-03-20 13:24:18 -0300

    Update title in layout.js to 'Forest Bar'

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 import "./globals.css";
 
 export const metadata = {
-  title: "Forest Bar Sales Log",
+  title: "Forest Bar",
   description: "Registro de vendas do Forest Bar",
   icons: {
     icon: "/forest-bar.png",
